c35769ac8e0a8d7d1e87fe6263430dfef05cab35
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-28 12:49:09 +0530

    Calculate wage for one day

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,20 +1,23 @@
 package employeewagecomputationprob;
 //program to calculate employee wage.
 public class EmployeeWageComputation {
-	static final int PRESENT=1;
+	static final int PRESENT=1,WAGE_PER_HOUR=20;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int Worked_Hrs=0, Day_Salary_Of_Employee=0;
 		System.out.println("Wecome to employee wage program");
   		int Random_No=(int) (Math.floor(Math.random()*10)%2);
-  		
+  		//checking employee is present or absent
   		if(PRESENT==Random_No)
-  		
+  		{
   			System.out.println("Employee is present.");
-  			
+  			Worked_Hrs=8;
+  		}	
   		else
   			System.out.println("Employee is absent.");
-
- 
+  		//computing employee wage for one day
+  		Day_Salary_Of_Employee=WAGE_PER_HOUR*Worked_Hrs;
+  		System.out.println("Your one day salary creadited to your account, rupees " +Day_Salary_Of_Employee);
 	}
 
 }
