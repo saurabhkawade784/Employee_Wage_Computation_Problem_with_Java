47a47fd3374aab5c02a124bf46cdf26426301201
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-28 15:11:42 +0530

    Calculating Employee wage For 100 Hrs and 20 Days

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,13 +1,17 @@
 package employeewagecomputationprob;
 //program to calculate employee wage.
 public class EmployeeWageComputation {
-	static final int PRESENT=1,WAGE_PER_HOUR=20,FULL_TIME=1,PART_TIME=0,WORKING_DAYS_IN_MONTH=20;
+	static final int PRESENT=1,WAGE_PER_HOUR=20,FULL_TIME=1,PART_TIME=0,WORKING_DAYS_IN_MONTH=20,MAX_WORKING_HRS=100;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		int Worked_Hrs=0, Day_Salary_Of_Employee=0,Month_Salary_Of_Employee=0;
+		int Emp_Worked_hrs=0,i,Salary,Total_Salary_Of_Employee=0;
 		System.out.println("Wecome to employee wage program");
   		//int Random_No=(int) (Math.floor(Math.random()*10)%2);
   		//checking employee is present or absent
+	for(i=1;i<=WORKING_DAYS_IN_MONTH && Emp_Worked_hrs<= MAX_WORKING_HRS;i++)
+	{
 		int Job_Type=(int) (Math.floor(Math.random()*10)%3);
   		switch(Job_Type)
   		{
@@ -22,7 +26,21 @@ public class EmployeeWageComputation {
   	  		default:
   	  			System.out.println("Employee is absent.");
   		}	
-  			 
+  		
+  		if(Emp_Worked_hrs==96||Emp_Worked_hrs==94)
+  		{
+  			if(Worked_Hrs==8)
+  				break;
+  		}
+  		else if(Emp_Worked_hrs==98)
+  		{
+  			break;
+  		}
+  		Emp_Worked_hrs+=Worked_Hrs;
+  		Salary=WAGE_PER_HOUR*Worked_Hrs;
+  		Total_Salary_Of_Employee+=Salary;
+  	}	
+		System.out.println("Total salary of employee for 100hrs and 20days, rupees " +Total_Salary_Of_Employee);
   		//computing employee wage for one day
   		Day_Salary_Of_Employee=WAGE_PER_HOUR*Worked_Hrs;
   		System.out.println("Your one day salary creadited to your account, rupees " +Day_Salary_Of_Employee);
