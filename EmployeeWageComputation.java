0728d7d9cf071fce17d94b1f646c40b91364999f
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-28 13:31:56 +0530

    Calculate part and full time employee wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,7 +1,7 @@
 package employeewagecomputationprob;
 //program to calculate employee wage.
 public class EmployeeWageComputation {
-	static final int PRESENT=1,WAGE_PER_HOUR=20;
+	static final int PRESENT=1,WAGE_PER_HOUR=20,FULL_TIME=1,PART_TIME=0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int Worked_Hrs=0, Day_Salary_Of_Employee=0;
@@ -10,10 +10,19 @@ public class EmployeeWageComputation {
   		//checking employee is present or absent
   		if(PRESENT==Random_No)
   		{
-  			System.out.println("Employee is present.");
-  			Worked_Hrs=8;
-  		}	
-  		else
+  	  		int Job_Type=(int) (Math.floor(Math.random()*10)%2);
+  	  		if(FULL_TIME==Job_Type)
+  	  		{
+  	  			System.out.println("Employee is present full time.");
+  	  			Worked_Hrs=8;
+  	  		}
+  	  		else
+  	  		{
+  	  			System.out.println("Employee is present part time.");
+	  			Worked_Hrs=4;
+  	  		}	
+  		}
+  	  	else
   			System.out.println("Employee is absent.");
   		//computing employee wage for one day
   		Day_Salary_Of_Employee=WAGE_PER_HOUR*Worked_Hrs;
