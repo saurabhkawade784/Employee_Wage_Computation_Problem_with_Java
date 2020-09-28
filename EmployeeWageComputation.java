8d107d5c38a9d80b5015ba7fb4e0c625bd39d17f
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-28 11:37:35 +0530

    Welcome message

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,11 @@
+package employeewagecomputationprob;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wecome to employee wage program");
+
+	}
+
+}
