b21ad19ad991d917e4136289da085376b20c5d47
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-10-05 19:04:58 +0530

    Calculating employee wage for multiple companies

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,86 +1,119 @@
 package employeewagecomputationprob;
-//program to show ability to save Total Wage for each company.
-import java.util.ArrayList;
-import java.util.Scanner;
-
+//program for manging employee for multiple companies
+//class for salary computation
 public class EmployeeWageComputation
 {
 	public static final int FULL_TIME=1,PART_TIME=2;
-	public static int COMPANY_COUNT=0;
-	private final int TOTAL_EMPLOYEE_WAGE,WAGE_PER_HRS,WORKING_DAYS_IN_MONTH,MAX_WORKING_HRS;
-	public String COMPANY_NAME;
-//Function to check employee present or absent or full_time or Part_time.
-	static int checkEmployeeAttendance()
+
+	private static int COMPANY_COUNT=0;
+	private CompanyEmpWage[] CompanyEmpWageArray;
+
+//Function for getting information about employee present or absent 
+	static int Attendance_Of_Employee()
 	{
 		int Present_Or_Absent=(int)Math.floor(Math.random()*10)%3;
-		//returns value of above arithmetic computation
 		return Present_Or_Absent;
 	}
-//use static function to check employee working hours
+//function for getting information about how much hours employee worked
 	static int getWorkingHrs(int Present_Or_Absent)
 	{
-		int Worked_Hrs;
+		int workingHrs;
 		switch(Present_Or_Absent)
 		{
 				case FULL_TIME:
-					Worked_Hrs=8;
+					workingHrs=8;
 					break;
 				case PART_TIME:
-					Worked_Hrs=8;
+					workingHrs=8;
 					break;
 				default:
-					Worked_Hrs=0;
+					workingHrs=0;
 		}
-		//returns value
 
-		return Worked_Hrs;
+		return workingHrs;
 	}
-//Function for calculating total employee wage
-	public int CalculateEmployeeWage()
+//Show Company employee wage information into array
+	public void CalculateWage()
 	{
-		int Worked_Hrs=0,salary,Total_Employee_Salary=0,totalWorkingHrs=0,day;
-		for(day=1;day<=this.WORKING_DAYS_IN_MONTH && totalWorkingHrs<=this.MAX_WORKING_HRS;day++)
+		for(int i=0;i<COMPANY_COUNT;i++)
+		{
+			CompanyEmpWageArray[i].setTotalWage(CalculateWage(CompanyEmpWageArray[i]));
+                        CompanyEmpWageArray[i].show();
+		}
+	}
+//Function for calculating Employee wage depending on how much hour employee actually work
+	public int CalculateWage(CompanyEmpWage CompanyEmpWage)
+	{
+		int workingHrs=0,salary,totalSalary=0,totalWorkingHrs=0,day;
+		for(day=1;day<=CompanyEmpWage.WORKING_DAYS_IN_MONTH && totalWorkingHrs<=CompanyEmpWage.MAX_WORKING_HRS;day++)
 		{
 
-			Worked_Hrs=getWorkingHrs(checkEmployeeAttendance());
+			workingHrs=getWorkingHrs(Attendance_Of_Employee());
 
 			if(totalWorkingHrs==96||totalWorkingHrs==94)
 			{
-				if (Worked_Hrs==8)
+				if (workingHrs==8)
 				break;
 			}
 			else if(totalWorkingHrs==98)
 			{
 				break;
 			}
-			totalWorkingHrs+=Worked_Hrs;
-			salary=this.WAGE_PER_HRS*Worked_Hrs;
-			Total_Employee_Salary+=salary;
+			totalWorkingHrs+=workingHrs;
+			salary=CompanyEmpWage.WAGE_PER_HRS*workingHrs;
+			totalSalary+=salary;
 		}
-		COMPANY_COUNT++;
-		return Total_Employee_Salary;
+
+		return totalSalary;
 
 
 	}
+//ArrayObject
+	public EmployeeWageComputation()
+	{
+		CompanyEmpWageArray=new CompanyEmpWage[5];
+	}
+//Store information about company into array.
+	private void addCompanyEmpWage(String Company,int WagePerHr,int Month,int MaxWorkingHrs)
+	{
+		CompanyEmpWageArray[COMPANY_COUNT]=new CompanyEmpWage(Company,WagePerHr,Month,MaxWorkingHrs);
+		COMPANY_COUNT++;
+	}
+//main function
+	public static void main(String[] args)
+	{
+		EmployeeWageComputation  EW=new EmployeeWageComputation();
+		EW.addCompanyEmpWage("DATAMETICA",20,30,100);
+		EW.addCompanyEmpWage("CLOUDERA",30,15,120);
+		EW.CalculateWage();
 
-	public EmployeeWageComputation(String Company,int WagePerHr,int Month,int MaxWorkingHrs)
+
+	}
+}
+class CompanyEmpWage
+{
+	public final int WAGE_PER_HRS,WORKING_DAYS_IN_MONTH,MAX_WORKING_HRS;
+	public int Total_Employee_Wage;
+	public String COMPANY_NAME;
+
+	public CompanyEmpWage(String Company,int WagePerHr,int Month,int MaxWorkingHrs)
 	{
 		this.COMPANY_NAME=Company;
 		this.WAGE_PER_HRS=WagePerHr;
 		this.WORKING_DAYS_IN_MONTH=Month;
 		this.MAX_WORKING_HRS=MaxWorkingHrs;
-		this.TOTAL_EMPLOYEE_WAGE=CalculateEmployeeWage();
-		this.show();
 	}
 
+	public void setTotalWage(int totalSalary)
+	{
+		this.Total_Employee_Wage=totalSalary;
+	}
+//Show total employee salary for perticular company
 	public void show()
 	{
-		System.out.println("Company Name : "+this.COMPANY_NAME+", Total Wage :"+this.TOTAL_EMPLOYEE_WAGE);
+		System.out.println("Company Name : "+this.COMPANY_NAME+", Total Wage :"+this.Total_Employee_Wage);
 	}
 
-	public static void main(String[] args)
-	{
-		EmployeeWageComputation EWc1=new EmployeeWageComputation("DataMetica",30,15,120);
-		EmployeeWageComputation EWc2=new EmployeeWageComputation("CloudEra",30,15,120);
-	}
 }
+
+
